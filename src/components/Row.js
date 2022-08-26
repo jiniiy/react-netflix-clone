92af748a980a6047d9790b3e185961c167763c7d
jinmo <jinmo_kim@skal.suntory.co.jp>
2022-08-26 22:45:08 +0900

    makingRowComponentsAndCSSStylingfor5RowCardComponents

---
+++ src/components/Row.js
@@ -0,0 +1,44 @@
+import axios from "../api/axios"; //우리가 api폴더에서 정의한 axios URL를 가져와야 한다! baseURL: "https://api.themoviedb.org/3"
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    //정보를 가져올떼, 서버에 API를 axios를 사용해서 비동기 요청한다!!!!
+    const request = await axios.get(fetchUrl);
+    console.log("request", request);
+    setMovies(request.data.results);
+  };
+
+  return (
+    <section className='row'>
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'>{"<<"}</span>
+        </div>
+        <div id={id} className='row__posters'>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className='slider__arrow-right'>
+          <span className='arrow'>{">>"}</span>
+        </div>
+      </div>
+    </section>
+  );
+}
