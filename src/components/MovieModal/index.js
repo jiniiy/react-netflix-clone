aaef659dc8f7bf597434b73a1faf8743d62afd27
jinmo <jinmo_kim@skal.suntory.co.jp>
2022-09-01 21:57:16 +0900

    MakingMovieModalUiContinueing&Styling

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -11,7 +11,32 @@ function MovieModal({ //MovieModal 컴포넌트의 props정의해주기(내려�
     vote_average,
     setModalOpen
 })  {
-    return <div></div>;
+    return <div className="presentation">
+        <div className="wrapper-modal">
+            <div className="modal">
+                <span onClick={() => setModalOpen(false)} className="modal-close">
+                    X
+                </span>
+                
+                <img 
+                    className="modal__poster-img"
+                    src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+                    alt="modal__poster-img"
+                />
+
+                <div className="modal__content">
+                    <p className="modal__details">
+                        <span className="modal__user_perc">100% for you</span>
+                        {release_date ? release_date : first_air_date}
+                    </p>
+
+                    <h2 className="modal__titile">{title? title: name}</h2>
+                    <p className="modal__overview"> 평점: {vote_average}</p>
+                    <p className="modal__overview"> {overview}</p>
+                </div>
+            </div>
+        </div>
+    </div>;
 }
 
 export default MovieModal;
