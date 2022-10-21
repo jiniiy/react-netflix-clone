63b3b3af9b15dc0954e0999537beeacd78cb672f
jinmo <jiniiy07@gmail.com>
2022-10-21 19:31:39 +0900

    Fix_Nav.js_onClick_WindowscreenChangeToNavigate()

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -34,17 +34,14 @@ export default function Nav() {
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png" //wikipedia imgLogo
         className="nav_logo"
-        onClick={() => window.location.reload()} //Nav를 클릭했을 경우 리로드!
+        onClick={() => navigate(`/`)} //Nav를 클릭했을 경우 리로드! // NetflixLogo눌렀을때 절대경로 홈화면 "/"로 돌아가도록 수정!
       />
-
-    
           <input value={searchValue} 
            onChange={handleChange} 
            className="nav__input" 
            type="text" 
            placeholder="영화를 검색해주세요."
           />
-
       <img                                                                            
         alt="User logged"
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
